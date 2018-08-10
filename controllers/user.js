7becff0f8198debf311dd34f252c259d40c5fad8
John <634408262@qq.com>
2018-08-10 14:55:57 +0800

    update

--- controllers/user.js
+++ controllers/user.js
@@ -4,9 +4,12 @@ exports.login = async function (ctx) {
   const { userName, passWord } = ctx.request.body
   const hasUser = await user.account.findOne({ "username": userName, "password": passWord })
   if (hasUser) {
-    ctx.body = { "message": "登陆成功","status":"true" }
+    this.session.user = {
+      "username": userName
+    }
+    ctx.body = { "message": "登陆成功", "status": "true" }
   } else {
-    ctx.body = { "message": "用户名或密码不对","status":"false" }
+    ctx.body = { "message": "用户名或密码不对", "status": "false" }
   }
 }
 

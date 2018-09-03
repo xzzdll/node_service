28bc25a53382a1deb85cd0ca232340451c33e81a
John <634408262@qq.com>
2018-09-03 13:45:46 +0800

    fix bug

--- router/index.js
+++ router/index.js
@@ -4,9 +4,9 @@ const artical = require('../controllers/artical.js')
 const say = require('../controllers/say.js')
 
 router.post('/login', user.login);
-router.post('/user/create', user.login)
-router.post('/user/delete', user.login)
-router.post('/user/list', user.login)
+router.post('/user/create', user.create)
+router.post('/user/delete', user.delete)
+router.post('/user/list', user.list)
 
 router.post('/artical/delete',artical.delete);
 router.post('/artical/edit',artical.edit);

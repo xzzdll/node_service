88feb98585c6b9ed4c993fb94eba62fc5fc194a2
John <634408262@qq.com>
2018-09-03 11:24:36 +0800

    add new user

--- controllers/user.js
+++ controllers/user.js
@@ -11,4 +11,52 @@ exports.login = async function (ctx) {
   }
 }
 
+exports.delete = async function (ctx) {
+  if (ctx.session.user === undefined) {
+    ctx.body = { "message": "未登录", "status": "false" }
+    return;
+  }
+  const { id } = ctx.request.body
+  const state = await user.account.remove({ "_id": id })
+  if (state.ok === 1 && state.n !== 0) {
+    ctx.body = { "message": "删除成功", "status": "true" }
+  } else {
+    ctx.body = { "message": "删除失败", "status": "false" }
+  }
+}
+
+exports.create = async function (ctx) {
+  if (ctx.session.user === undefined) {
+    ctx.body = { "message": "未登录", "status": "false" }
+    return;
+  }
+  const { userName, passWord } = ctx.request.body
+  const newSay = await user.account.create({ "username": userName, "password": passWord })
+  if (newSay._id) {
+    ctx.body = { "message": "新增成功", "status": "true" }
+  } else {
+    ctx.body = { "message": "新增失败", "status": "false" }
+  }
+}
+
+exports.list = async function (ctx) {
+  if (ctx.session.user === undefined) {
+    ctx.body = { "message": "未登录", "status": "false" }
+    return;
+  }
+  let says
+  const { id } = ctx.request.body
+  if (id) {
+    says = await user.account.find({ "_id": id })
+  }
+  else {
+    says = await user.account.find({})
+  }
+  if (says.length !== 0) {
+    ctx.body = { "message": "查询成功", "status": "true", "list": says }
+  } else {
+    ctx.body = { "message": "查询失败", "status": "false" }
+  }
+}
+
 
--- router/index.js
+++ router/index.js
@@ -3,20 +3,18 @@ const user = require('../controllers/user.js')
 const artical = require('../controllers/artical.js')
 const say = require('../controllers/say.js')
 
-router.post('/login', user.login)
+router.post('/login', user.login);
+router.post('/user/create', user.login)
+router.post('/user/delete', user.login)
+router.post('/user/list', user.login)
 
 router.post('/artical/delete',artical.delete);
-
 router.post('/artical/edit',artical.edit);
-
 router.post('/artical/create',artical.create);
-
 router.post('/artical/list',artical.list);
 
 router.post('/say/delete',say.delete);
-
 router.post('/say/list',say.list);
-
 router.post('/say/create',say.create);
 
 module.exports = router

3aa54724ca6d7ce8e87645323d743034bd5f174a
John <634408262@qq.com>
2018-10-25 10:32:57 +0800

    增加微信验证接口

---
+++ controllers/webchat.js
@@ -0,0 +1,23 @@
+const sha1 = require("sha1");
+
+let config = {
+  wechat: {
+    appID: "wxe0d8ad6a6bf9a3bb",
+    appsecret: "1fb18b92f2e6bb446871c8a44d0a000d",
+    token: "john"
+  }
+};
+
+exports.check = async function(ctx) {
+  const { signature, timestamp, echostr, nonce } = ctx.request.body;
+
+  let token = config.wechat.token;
+  let str = [token, timestamp, nonce].sort().join("");
+  let sha = sha1(str);
+
+  if (sha === signature) {
+    ctx.body = echostr + "";
+  } else {
+    ctx.body = "wrong";
+  }
+};
